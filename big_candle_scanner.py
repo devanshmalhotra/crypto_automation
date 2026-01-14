ba39b8da2f1742a477f0d23faa16649651cb81db
devanshmalhotra <69192215+devanshmalhotra@users.noreply.github.com>
2026-01-14 22:34:59 +0530

    Update big_candle_scanner.py

--- big_candle_scanner.py
+++ big_candle_scanner.py
@@ -25,7 +25,7 @@ USE_ATR_FILTER = True
 # EMAIL CONFIG (ENV VARS)
 # =========================
 sender_email = "devanshmalhotra98@gmail.com"
-sender_password = "rgcsqglobazxvgt"  # Use app password if Gmail 2FA is on
+sender_password = "cigl vjac hfxl wrwv"  # Use app password if Gmail 2FA is on
 receiver_email = "devanshmalhotra98@gmail.com"
 
 # =========================
